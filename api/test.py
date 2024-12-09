2c220ea80dc53c9c142bd0823ac950b469c4343f
dmiv <dmitriy40647274@gmail.com>
2024-12-09 16:21:35 +0300

    predefined data

--- api/test.py
+++ api/test.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-
+from sqlalchemy import select
+from models.user import User
+from enum import Enum
 router = APIRouter()
 
 
@@ -10,3 +12,21 @@ async def main():
 @router.get("/second")
 async def second():
     return "helloworld"
+
+@router.get("/orm")
+def get_orm() -> str:
+    stmt = select(User)
+    return str(stmt)
+
+@router.get("/items/{item_id}")
+def get_item_by_id(item_id: int | float):
+    return {"item_id": item_id}
+
+
+class PredefinedUsernames(Enum):
+    username1 = "dmiv"
+    username2 = "tyrannozavr"
+
+@router.get("/predefined/{username}")
+def get_predefined_parameter(username: PredefinedUsernames):
+    return f"Hello, {username.value}"
